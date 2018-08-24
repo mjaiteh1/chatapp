df0ed9876a224c1bbd2cb6d0d7d77c944694fcbb
Mariama Jaiteh <mjaiteh@smith.edu>
2018-08-23 20:02:02 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chatapp';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule} from 'angularfire2/database';
+import { HttpModule } from '@angular/http';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBUcXEJRBZP7uLGBvla959v0wejvpRBY4c",
+    authDomain: "chatapp-c8a29.firebaseapp.com",
+    databaseURL: "https://chatapp-c8a29.firebaseio.com",
+    projectId: "chatapp-c8a29",
+    storageBucket: "chatapp-c8a29.appspot.com",
+    messagingSenderId: "812579762937"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
